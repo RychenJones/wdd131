8ff6b04141fa5ad4b74bb4e3d2f3b6f37666fc32
RychenJones <rychenjones66@gmail.com>
2026-01-12 12:29:50 -0700

    test

--- ponder/week-2/dom-basics.js
+++ ponder/week-2/dom-basics.js
@@ -3,7 +3,7 @@ console.log(title);
 title.textContent = 'Web Page Components';
 
 let topic_title = document.getElementById('topics');
-topic_title.style.color = 'red';
+topic_title.style.color = 'purple';
 
 let list = document.querySelector('.list');
 list.style.border = '3px solid black';
@@ -20,4 +20,8 @@ let selectElem = document.getElementById('webdevlist');
 selectElem.addEventListener('change', function(){
     let codeValue = selectElem.value;
     console.log(codeValue);
+    document.querySelector('#html').style.color = 'red';
 })
+
+
+const newPara = document.createElement('p')
