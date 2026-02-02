2c73cae6d48beebc3156d51a944c4e10c4bb0c29
RychenJones <rychenjones66@gmail.com>
2026-02-02 08:57:57 -0700

    stuff

--- coolpics/index.js
+++ coolpics/index.js
@@ -12,11 +12,9 @@ const modal = document.querySelector('dialog');
 const modalImage = modal.querySelector('img');
 const closeButton = modal.querySelector('.close-viewer');
 
-// Event listener for opening the modal
 images.addEventListener('click', openModal);
 
 function openModal(e) {
-// Code to show modal  - Use event parameter 'e'   
     const img = e.target;
 
     const src = img.getAttribute('src');
@@ -29,12 +27,10 @@ function openModal(e) {
     modal.showModal();
 }
 
-// Close modal on button click
 closeButton.addEventListener('click', () => {
     modal.close();
 });
 
-// Close modal if clicking outside the image
 modal.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.close();
